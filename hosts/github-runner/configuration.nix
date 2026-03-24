31c5f9f6e5d81e03ddef913edb8755bd87f8a316
gjermundhp02 <gjermund.pedersen@gmail.com>
2026-03-24 22:01:43 +0100

    fix: update runner

--- hosts/github-runner/configuration.nix
+++ hosts/github-runner/configuration.nix
@@ -1,26 +1,11 @@
 {...}:{
-  fileSystems."/mnt/truenas" = {
-    device = "192.168.1.107:/mnt/storage0/iso";
-    fsType = "nfs";
-  };
-  systemd.tmpfiles.settings = {
-    mnt = {
-      "/mnt/truenas" = {
-        d = {
-          group = "root";
-          mode = "0755";
-          user = "root";
-        };
-      };
-    };
-  };
   # optional, but ensures rpc-statsd is running for on demand mounting
-  boot.supportedFilesystems = [ "nfs" ];
   services.github-runners = {
     proxmox1 = {
-      url = "https://github.com/Login-Linjeforening-for-IT";
+      url = "https://github.com/login-linjeforeningen-for-it";
       name = "Proxmox 1";
       enable = true;
+      extraLabels = [ "nixos" ];
       tokenFile = "/var/lib/github-runner/token";
     };
   };
